46e4ee294b4a43439a98d624539cedbd9c605260
only1isus <only1isus@gmail.com>
2018-10-29 19:54:10 -0500

    added the sensordata struct

--- util/util.go
+++ util/util.go
@@ -1,18 +1,25 @@
 package util
 
-// Log defines the structure of the log entry s
+// Log
 type Log struct {
 	Time    int64
 	Success bool
 	Message string
 }
 
-// User - this is the structure of the users info to be stored in the database
+// User
 type User struct {
 	name  string
 	email string
 	phone int64
 	role  string
+}
+
+// Sensordata
+type Sensordata struct {
+	time       int64
+	sensortype string
+	value      string
 }
 
 // PlaceHolder stores the structured data to be used after Decode function call
