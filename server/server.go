dcd3846d76901f1307f6abf9071c0b30d010fbdd
only1isus <only1isus@gmail.com>
2018-12-02 19:21:41 -0500

    added endpoints logs and change settings

--- server/server.go
+++ server/server.go
@@ -46,6 +46,7 @@ func postUser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// returns the sensor data by type
 func getSensorData(w http.ResponseWriter, r *http.Request) {
 
 	device := r.URL.Path[len("/api/sensor/"):]
@@ -64,6 +65,51 @@ func getSensorData(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// get all the logs
+func getLogs(w http.ResponseWriter, r *http.Request) {
+	logs, err := util.GetLogs()
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), 500)
+	}
+	l, err := json.Marshal(logs)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), 500)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(l)
+	}
+}
+
+// changeSettings edits the config file of the system
+func changeSettings(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("trying to change settings")
+	if err := r.ParseForm(); err != nil {
+		fmt.Println(err)
+	}
+
+	var u util.User
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if err := json.Unmarshal(body, &u); err != nil {
+		fmt.Println(err)
+	}
+	err = u.Add()
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Println(err)
+	} else {
+		w.Header()
+		w.WriteHeader(200)
+	}
+
+}
+
 func main() {
 	allowedHeaders := handlers.AllowedHeaders([]string{"application/json", "application/x-www-form-urlencoded", "multipart/form-data"})
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
@@ -73,6 +119,8 @@ func main() {
 	log.Printf("server running pn port %s...", port)
 	router.HandleFunc("/newuser", postUser).Methods("POST")
 	router.HandleFunc("/api/sensor/{type}", getSensorData).Methods("GET")
+	router.HandleFunc("/api/logs", getLogs).Methods("GET")
+	router.HandleFunc("/api/settings", changeSettings).Methods("POST")
 	http.ListenAndServe(port, handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(router))
 
 }
