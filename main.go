d43976ac1c1aaa76f47a4b029dca231184150099
only1isus <only1isus@gmail.com>
2018-12-14 13:59:31 -0500

    handle ^C interupt gracefully

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/only1isus/majorProj/control"
 )
@@ -17,24 +19,47 @@ func welcome() {
                     ,   #          '
                     '              *		
                     '              '  
-                     '-,___.'__,.-'
-    ==============================================
-                   AMBOROSA - alpha
-                          %v
-    ==============================================
-    
-    `, version)
+					'-,___.'__,.-'
+					==============================================
+					AMBOROSA - alpha
+					%v
+					==============================================
+					
+					`, version)
 }
 
 func main() {
+	c := make(chan os.Signal, 1)
+	kill := make(chan bool)
 	welcome()
 	public := flag.Bool("public", false, "if set to true then the system will make the info public")
 	if *public {
 		fmt.Println("The system is public.")
 	}
 
-	fan := control.Fan{}
-	fan.On()
-	time.Sleep(3 * time.Second)
-	fan.Off()
+	d := control.Devices{}
+	fan, err := d.Get("fan")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer fan.GracefulKill(kill)
+	t := control.Temperature{}
+	if err := t.Maintain(30, fan); err != nil {
+		fmt.Println(err)
+	}
+
+	// cleaning up. Closing all pins and turning off all devices whenever Ctrl ^ C is recieved.
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		kill <- true
+	}()
+	select {
+	case _ = <-kill:
+		fan.Off()
+		fmt.Println("cleaned!")
+		os.Exit(0)
+	}
+
+	// go server.Serve()
 }
