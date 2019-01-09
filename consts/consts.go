a694ece2e1c1cf3b2d728a4ab21c3d67417cc537
only1isus <only1isus@gmail.com>
2019-01-09 01:09:28 -0500

    constants defined

---
+++ consts/consts.go
@@ -0,0 +1,17 @@
+package consts
+
+type BucketName string
+type BucketFilter string
+
+const (
+	Temperature BucketFilter = "temperature"
+	Humidity    BucketFilter = "humidity"
+	PH          BucketFilter = "ph"
+	EC          BucketFilter = "ec"
+	WaterLevel  BucketFilter = "waterlevel"
+	All         BucketFilter = ""
+
+	Sensor BucketName = "sensor"
+	User   BucketName = "user"
+	Log    BucketName = "log"
+)
