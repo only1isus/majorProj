d10c98f09071aba93c16d8b09d6d45c2a9fd0e88
only1isus <only1isus@gmail.com>
2019-01-09 21:28:56 -0500

    changed endpoints to be compatible with the database

--- server/server.go
+++ server/server.go
@@ -6,39 +6,43 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/only1isus/majorProj/util"
+	"github.com/only1isus/majorProj/consts"
+	"github.com/only1isus/majorProj/database"
+	"github.com/only1isus/majorProj/types"
 )
 
 const (
-	host     string = "localhost"
-	grpcPort string = ":8081"
-	port     string = ":8080"
+	host string = "localhost"
+	port string = ":8080"
 )
-
-// Userinfo defines the user data structure
-// type user util.User
 
 func postUser(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Println(err)
 	}
 
-	var u util.User
+	var u types.User
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println(err)
+		w.WriteHeader(500)
+		w.Write([]byte("could not parse the data sent"))
 	}
 
 	if err := json.Unmarshal(body, &u); err != nil {
-		fmt.Println(err)
+		w.WriteHeader(500)
+		w.Write([]byte("something went wrong while getting the information"))
+
 	}
-	err = u.Add()
+	u.CreatedAt = time.Now().Unix()
+	err = db.AddEntry(consts.User, []byte(u.Email), u)
 	if err != nil {
 		w.WriteHeader(500)
+		w.Write([]byte("something went wrong while getting the information"))
 		fmt.Println(err)
 	} else {
 		w.Header()
@@ -46,12 +50,7 @@ func postUser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// returns the sensor data by type
-func getSensorData(w http.ResponseWriter, r *http.Request) {
-
-	device := r.URL.Path[len("/api/sensor/"):]
-	device = strings.ToLower(device)
-	data, err := util.GetSensorDataByType(device)
+func sendResponse(w http.ResponseWriter, data interface{}) {
 	d, err := json.Marshal(data)
 	if err != nil {
 		fmt.Printf("got an error while trying encode response")
@@ -62,25 +61,59 @@ func getSensorData(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write(d)
 	}
+}
 
+// returns the sensor data by type
+func getSensorData(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	span := query.Get("timespan")
+	sensorType := query.Get("sensortype")
+	s, err := strconv.Atoi(span)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("please check the timespan parameter passed"))
+		return
+	}
+	switch sensorType {
+	case "temperature":
+		data, err := db.GetSensorData(consts.Temperature, int64(s))
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("something went wrong trying to get the data requested"))
+			return
+		}
+		sendResponse(w, data)
+	case "all":
+		data, err := db.GetSensorData(consts.All, int64(s))
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("something went wrong trying to get the data requested"))
+			return
+		}
+		sendResponse(w, data)
+	default:
+		w.WriteHeader(404)
+		w.Write([]byte("404 page not found"))
+		return
+	}
 }
 
 // get all the logs
 func getLogs(w http.ResponseWriter, r *http.Request) {
-	logs, err := util.GetLogs()
+	query := r.URL.Query()
+	span := query.Get("timespan")
+	s, err := strconv.Atoi(span)
 	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), 500)
+		w.WriteHeader(500)
+		w.Write([]byte("please check the timespan parameter passed"))
+		return
 	}
-	l, err := json.Marshal(logs)
+	logs, err := db.GetLogs(int64(s))
 	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), 500)
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		w.Write(l)
+		w.WriteHeader(500)
+		w.Write([]byte("something went wrong trying to get the data requested"))
 	}
+	sendResponse(w, logs)
 }
 
 // changeSettings edits the config file of the system
@@ -89,25 +122,6 @@ func changeSettings(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Println(err)
 	}
-
-	var u util.User
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if err := json.Unmarshal(body, &u); err != nil {
-		fmt.Println(err)
-	}
-	err = u.Add()
-	if err != nil {
-		w.WriteHeader(500)
-		fmt.Println(err)
-	} else {
-		w.Header()
-		w.WriteHeader(200)
-	}
-
 }
 
 func main() {
@@ -118,9 +132,8 @@ func main() {
 	router := mux.NewRouter()
 	log.Printf("server running pn port %s...", port)
 	router.HandleFunc("/newuser", postUser).Methods("POST")
-	router.HandleFunc("/api/sensor/{type}", getSensorData).Methods("GET")
-	router.HandleFunc("/api/logs", getLogs).Methods("GET")
+	router.HandleFunc("/api/sensor/", getSensorData).Methods("GET")
+	router.HandleFunc("/api/logs/", getLogs).Methods("GET")
 	router.HandleFunc("/api/settings", changeSettings).Methods("POST")
 	http.ListenAndServe(port, handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(router))
-
 }
