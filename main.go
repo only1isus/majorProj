9321f7b6fd385b4fb1872c01cfb9d32266f8202e
only1isus <only1isus@gmail.com>
2019-03-20 12:42:33 -0500

    testing functions

--- main.go
+++ main.go
@@ -9,11 +9,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/only1isus/majorProj/consts"
-	"github.com/only1isus/majorProj/types"
-
 	"github.com/only1isus/majorProj/control"
 	"github.com/only1isus/majorProj/rpc"
+	"github.com/only1isus/majorProj/types"
 )
 
 func welcome() {
@@ -40,49 +38,31 @@ func main() {
 	notification := make(chan []byte, 1)
 	kill := make(chan bool, 1)
 	onTime := time.Now()
+	entry := make(chan *types.LogEntry, 1)
 	log.Println("System running")
 
-	temperature := control.Temperature{}
-	Humidity := control.Humidity{}
-	fan, err := control.NewOutputDevice(consts.CoolingFan)
-
-	go func(temp *control.Temperature, humid *control.Humidity) {
-		for {
-			timer := time.NewTimer(time.Minute * 5)
-			defer timer.Stop()
-			// wait for the timer to reach its limit
-			<-timer.C
-
-			t, err := temp.Prepare()
-			if err != nil {
-				log.Println("got and error from prepare method", err)
-			}
-
-			if err := rpc.CommitSensorData(t); err != nil {
-				log.Println("cannot send data to the database server", err)
-			}
-		}
-	}(&temperature, &Humidity)
-
-	if err != nil {
-		log.Println("got an error creating the fan output device ", err)
-	}
-	err = temperature.Maintain(30, fan, notification)
-
-	// cleaning up. Closing all pins and turning off all devices whenever Ctrl ^ C is recieved.
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	gl, err := control.NewGrowLight()
+	if err != nil {
+		fmt.Println(err)
+	}
+	go gl.TurnOnThenWait()
+
+	wl, err := control.NewWaterLevelSensor(0x48, 1)
+	if err != nil {
+		fmt.Printf("got an error creating the water level sensor %v", err)
+	}
+
+	go wl.CheckAndNotify(3, entry)
 
 	go func() {
 		for {
 			select {
-			case n, ok := <-notification:
-				if !ok || n == nil {
-					log.Println("something went wrong getting the nofifications")
-				}
-				err := rpc.CommitLog(&n)
-				if err != nil {
-					log.Println("got an error from the commit log ", err)
-				}
+			case en := <-entry:
+				fmt.Printf("%v\n", *en)
+				// case _ = <-c:
+				// 	kill <- true
 			}
 		}
 	}()
@@ -93,10 +73,8 @@ func main() {
 
 	<-kill
 	log.Println("cleaning up")
-	err = fan.Off()
-	if err != nil {
-		fmt.Println("got an error from")
-	}
+	gl.Off()
+	// wl.Close()
 	msg := types.LogEntry{
 		Message: fmt.Sprintf("System terminated from the command line at %v on %v. On time %v minutes.", time.Now().Format("15:04:05"), time.Now().Format("2006-01-02"), int64(time.Now().Sub(onTime).Minutes())),
 		Success: true,
