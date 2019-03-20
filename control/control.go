dd8b47af527670988a07a70b6e469c0c9c73c7d7
only1isus <only1isus@gmail.com>
2019-03-20 12:39:59 -0500

    moved the methods related to types to seperate types

--- control/control.go
+++ control/control.go
@@ -1,17 +1,16 @@
 package control
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"time"
+	"strings"
+
+	"gobot.io/x/gobot/drivers/i2c"
+	"gobot.io/x/gobot/platforms/raspi"
 
 	"github.com/ghodss/yaml"
 	"github.com/only1isus/majorProj/config"
 	"github.com/only1isus/majorProj/consts"
-	"github.com/only1isus/majorProj/types"
 	"github.com/stianeikeland/go-rpio"
-	"github.com/yryz/ds18b20"
 )
 
 // Devices is a slice of all the output devices.
@@ -36,11 +35,16 @@ type DriverPins struct {
 	IN2 uint8 `yaml:"in2"`
 }
 
-// ADC AS1115 ADC module
-type ADC struct {
-	Name string
-	Addr []string
-	Info string
+type ADCSensor struct {
+	Name       consts.AnalogSensor `yaml:"name"`
+	Every      int64               `yaml:"every"`
+	AnalogPin  int                 `yaml:"analogPin"`
+	connection *i2c.ADS1x15Driver
+}
+
+// ADCSensor is a struct of the input sensor connected to the ADS1115 module.
+type ADCSensors struct {
+	AnalogSensor []ADCSensor `yaml:"analogSensor"`
 }
 
 // type CirculationPump OutputDevice
@@ -52,19 +56,45 @@ type Sensor struct {
 	value float64
 	Pin   int
 }
-
-// Temperature ...
-type Temperature Sensor
 type Humidity Sensor
 
-type PH ADC
-type EC ADC
-type WaterLevel ADC
+// type WaterLevel Sensor
 
-type AnalogSensors interface {
-	Get()
-	Save()
-	Log()
+// type PH ADCSensor
+// type EC ADCSensor
+type WaterLevel ADCSensor
+
+// type AnalogSensors interface {
+// 	Get()
+// 	Save()
+// 	Log()
+// }
+
+// I2CConnection returns a ADS1115 i2c connection
+func i2cConnection(address, bus int) (*i2c.ADS1x15Driver, error) {
+	a := raspi.NewAdaptor()
+	ads := i2c.NewADS1115Driver(a, i2c.WithBus(bus), i2c.WithAddress(address))
+	ads.DefaultGain, _ = ads.BestGainForVoltage(5.0)
+	return ads, nil
+}
+
+func NewAnalogSensor(sensorName consts.AnalogSensor) (*ADCSensor, error) {
+	inputSensors := new(ADCSensors)
+	configFile, err := config.ReadConfigFile()
+	if err != nil {
+		return nil, err
+	}
+	if err := yaml.Unmarshal(configFile, inputSensors); err != nil {
+		fmt.Println("error unmarshalling", err)
+		return nil, err
+	}
+	for _, sensor := range inputSensors.AnalogSensor {
+		if strings.ToLower(string(sensor.Name)) == strings.ToLower(string(sensorName)) {
+			return &sensor, nil
+		}
+		return nil, fmt.Errorf("cannot find the sensor in the config file")
+	}
+	return nil, nil
 }
 
 // Get reads the config file and returns a list of nodes and error.
@@ -87,125 +117,6 @@ func (d *Devices) Get(deviceName consts.OutputDevice) (*OutputDevice, error) {
 	return nil, nil
 }
 
-// Get method when called returns the current temperature.
-func (t *Temperature) Get() (*float64, error) {
-	sensors, err := ds18b20.Sensors()
-	if err != nil {
-		panic(err)
-	}
-
-	for _, sensor := range sensors {
-		var err error
-		t.value, err = ds18b20.Temperature(sensor)
-		if err != nil {
-			return nil, err
-		}
-		break
-	}
-	return &t.value, err
-}
-
-// Prepare gets the entry ready to be committed to the database
-func (t *Temperature) Prepare() (*[]byte, error) {
-	temp, err := t.Get()
-	if err != nil {
-		return nil, err
-	}
-	entry := types.SensorEntry{
-		Time:       time.Now().Unix(),
-		SensorType: consts.Temperature,
-		Value:      *temp,
-	}
-	data, err := json.Marshal(entry)
-	if err != nil {
-		return nil, err
-	}
-	return &data, nil
-}
-
-// Maintain method tries to keep the temperature at the value passed to the method.
-func (t *Temperature) Maintain(value float64, fan *OutputDevice, notify chan<- []byte) error {
-	go func(n chan<- []byte) error {
-		for {
-			temp, err := t.Get()
-			if err != nil {
-				n <- nil
-				return err
-			}
-			val := *temp
-			if val >= value {
-				log.Printf("Turning on fan. Current temperature is %f, limit set to %f", val, value)
-				if err := fan.On(); err != nil {
-					n <- nil
-					return err
-				}
-				onTime := time.Now()
-				for {
-					currentTemp, err := t.Get()
-					if err != nil {
-						n <- nil
-						return err
-					}
-					if value >= *currentTemp {
-						time.Sleep(1 * time.Minute)
-						log.Println("Done")
-						fan.ChangePWM(0.4)
-						time.Sleep(1 * time.Minute)
-						if err := fan.Off(); err != nil {
-							n <- nil
-							return err
-						}
-						msg := types.LogEntry{
-							Message: fmt.Sprintf("Fan was turned on for %v minute(s). Upper limit set to %vc", int64(time.Now().Sub(onTime).Minutes()), value),
-							Success: true,
-							Time:    time.Now().Unix(),
-							Type:    "control",
-						}
-						out, err := json.Marshal(msg)
-						if err != nil {
-							n <- nil
-							return err
-						}
-						n <- out
-						break
-					}
-				}
-			}
-			time.Sleep(30 * time.Second)
-		}
-	}(notify)
-	return nil
-}
-
-func (h *Humidity) Get() (*float64, error) {
-	// sensorType := dht.DHT11
-	// _, humidity, _, err := dht.ReadDHTxxWithRetry(sensorType, 4, false, 10)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// hum := float64(humidity)
-	// return &hum, err
-	return nil, nil
-}
-
-func (h *Humidity) Prepare() (*[]byte, error) {
-	hum, err := h.Get()
-	if err != nil {
-		return nil, err
-	}
-	entry := types.SensorEntry{
-		Time:       time.Now().Unix(),
-		SensorType: consts.Humidity,
-		Value:      *hum,
-	}
-	data, err := json.Marshal(entry)
-	if err != nil {
-		return nil, err
-	}
-	return &data, nil
-}
-
 // NewOutputDevice takes the name of the device and returns an instance of that device.
 // Note: to avoid errors check that the name of device in the config file is the same as
 // the ones in consts.go.
@@ -218,9 +129,7 @@ func NewOutputDevice(name consts.OutputDevice) (*OutputDevice, error) {
 	return device, nil
 }
 
-// On method turns the fan on.
 func (o OutputDevice) On() error {
-
 	err := rpio.Open()
 	if err != nil {
 		return err
@@ -239,8 +148,34 @@ func (o OutputDevice) On() error {
 
 	en.Freq(1920000)
 	en.DutyCycle(uint32(o.Rate*128), 128)
-	in1.Low()
-	in2.High()
+	in1.High()
+	in2.Low()
+
+	return nil
+}
+
+func (o OutputDevice) OnNoPWM() error {
+
+	err := rpio.Open()
+	if err != nil {
+		return err
+	}
+
+	defer rpio.Close()
+
+	en := rpio.Pin(o.Pins.EN)
+	in1 := rpio.Pin(o.Pins.IN1)
+	in2 := rpio.Pin(o.Pins.IN2)
+
+	en.Output()
+	in1.Output()
+	in2.Output()
+
+	en.Freq(1920000)
+	en.DutyCycle(uint32(o.Rate*128), 128)
+	en.High()
+	in1.High()
+	in2.Low()
 
 	return nil
 }
