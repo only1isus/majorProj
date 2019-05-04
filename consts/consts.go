59dd982b2f0ccfa74411a5ac6b7f2a4e53a9d88d
only1isus <only1isus@gmail.com>
2019-05-04 01:37:12 -0400

    updated

--- consts/consts.go
+++ consts/consts.go
@@ -5,6 +5,10 @@ type OutputDevice string
 type BucketFilter string
 type AnalogSensor string
 
+type I2CSensor string
+
+type ADS1115Device string
+
 const (
 	Temperature BucketFilter = "temperature"
 	Humidity    BucketFilter = "humidity"
@@ -24,4 +28,11 @@ const (
 	GrowLight       OutputDevice = "growlight"
 
 	WaterLevelSensor AnalogSensor = "waterlevel"
+	PHSensor         AnalogSensor = "ph"
+
+	Sth3xTemperature I2CSensor = "sth3xtemperature"
+	Sth3xHumidity    I2CSensor = "sth3xhumidity"
+
+	ADS1115Device1 ADS1115Device = "ads1115_1"
+	ADS1115Device2 ADS1115Device = "ads1115_2"
 )
