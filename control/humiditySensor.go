baccf0a5930cb5f379c47b02d5a54f040a450fd8
only1isus <only1isus@gmail.com>
2019-05-04 01:24:16 -0400

    initial commit

--- control/humiditySensor.go
+++ control/humiditySensor.go
@@ -2,43 +2,75 @@ package control
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"time"
 
+	i2c "github.com/d2r2/go-i2c"
+	sht3x "github.com/d2r2/go-sht3x"
+
 	"github.com/only1isus/majorProj/consts"
+	"github.com/only1isus/majorProj/rpc"
 	"github.com/only1isus/majorProj/types"
 )
 
-type HumiditySensor Sensor
+type HumiditySensor I2CSensor
 
-func NewHumiditySensor() HumiditySensor {
-	return HumiditySensor{}
+func NewHumiditySensor() (*HumiditySensor, error) {
+	humiditySensor, err := NewI2CSensor(consts.Sth3xHumidity)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(*humiditySensor)
+	hs := HumiditySensor(*humiditySensor)
+	return &hs, nil
 }
 
 func (h *HumiditySensor) Get() (*float64, error) {
-	// sensorType := dht.DHT11
-	// _, humidity, _, err := dht.ReadDHTxxWithRetry(sensorType, 4, false, 10)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	fmt.Println("reading humidity")
+	time.Sleep(time.Second * 1)
+	i2cconn, err := i2c.NewI2C(h.Address, h.Bus)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer i2cconn.Close()
 
-	// hum := float64(humidity)
-	// return &hum, err
-	return nil, nil
+	sensor := sht3x.NewSHT3X()
+	_, humidity, err := sensor.ReadTemperatureAndRelativeHumidity(i2cconn, sht3x.RepeatabilityLow)
+	if err != nil {
+		return nil, err
+	}
+	hum := new(float64)
+	*hum = ToFixed(float64(humidity), 1)
+	return hum, nil
 }
 
-func (h *HumiditySensor) Prepare() (*[]byte, error) {
-	hum, err := h.Get()
-	if err != nil {
-		return nil, err
+func (h *HumiditySensor) ReadAndCommit() error {
+	for {
+		timer := time.NewTimer(time.Minute * time.Duration(h.Every))
+		defer timer.Stop()
+		// wait for the timer to reach its limit
+		<-timer.C
+
+		hum, err := h.Get()
+		if err != nil {
+			return err
+		}
+		out := &types.SensorEntry{
+			Time:       time.Now().Unix(),
+			SensorType: consts.Humidity,
+			Value:      *hum,
+		}
+		data, err := json.Marshal(out)
+		if err != nil {
+			return err
+		}
+		if err := rpc.CommitSensorData(&data); err != nil {
+			return err
+		}
 	}
-	entry := types.SensorEntry{
-		Time:       time.Now().Unix(),
-		SensorType: consts.Humidity,
-		Value:      *hum,
-	}
-	data, err := json.Marshal(entry)
-	if err != nil {
-		return nil, err
-	}
-	return &data, nil
+}
+
+func (h HumiditySensor) Close() error {
+	return h.connection.Close()
 }
